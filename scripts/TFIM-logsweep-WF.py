reorganize data and fix normalization issue in scripts

scripts/TFIM-logsweep-WF.py
@@ -27,6 +27,7 @@ from qdclib import qdccirq
 from qdclib import spinmodels
 
 
+# Prepare file paths
 cooling_dir = os.path.join(data_dir, "cooling")
 reheating_dir = os.path.join(data_dir, "reheating")
 os.makedirs(cooling_dir, exist_ok=True)
@@ -34,7 +35,7 @@ os.makedirs(reheating_dir, exist_ok=True)
 outfile_cooling = os.path.join(cooling_dir, f"L{L}JvB{JvB}K{K}.json")
 outfile_reheating = os.path.join(reheating_dir, f"L{L}JvB{JvB}K{K}.json")
 
-
+# Check existance of output to avoid repeating simulations.
 reheating_out_exists = False
 if os.path.exists(outfile_cooling):
     print(f'output file {outfile_cooling} exists already.')
@@ -87,8 +88,8 @@ if not cooling_out_exists:
         norm_samples.append(np.float64(
             norm
         ))  # conversion to np.float64 needed for compatibility with json.
-        energy_samples.append(system.energy_expval(final_state) / norm)
-        gsf_samples.append(system.ground_space_fidelity(final_state) / norm)
+        energy_samples.append(system.energy_expval(final_state) / norm**2)
+        gsf_samples.append(system.ground_space_fidelity(final_state) / norm**2)
 
     out_dict = dict(
         L=L,
@@ -139,8 +140,8 @@ if not reheating_out_exists:
             norm
         ))  # conversion to np.float64 needed for compatibility with json.
 
-        energy_samples.append(system.energy_expval(final_state) / norm)
-        gsf_samples.append(system.ground_space_fidelity(final_state) / norm)
+        energy_samples.append(system.energy_expval(final_state) / norm**2)
+        gsf_samples.append(system.ground_space_fidelity(final_state) / norm**2)
 
     out_dict = dict(
         L=L,